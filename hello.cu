f4a095828d82198f5d1142229d219cb7775cb0d3
gbettonte <126325948+gbettonte@users.noreply.github.com>
2024-12-04 19:19:02 +0100

    Update hello.cu

--- hello.cu
+++ hello.cu
@@ -31,28 +31,27 @@ int main() {
     int global_minimum_cpu[1]; global_minimum_cpu[0] = INT_MAX; std::cout << "Initial max number: " << global_minimum_cpu[0] << std::endl;
     
     
-    // Allocate memory for global minimum on GPU
+    // Allocate memory for global minimum on GPU; two variables: one for each approach
     int *global_minimum_gpu_opt; int *global_minimum_gpu_fix;
-    cudaMalloc(&global_minimum_gpu_opt, sizeof(int)); cudaMalloc(&global_minimum_gpu_fix, sizeof(int)); 
+    cudaMalloc(&global_minimum_gpu_opt, sizeof(int)); cudaMalloc(&global_minimum_gpu_fix, sizeof(int));
+    //The follwing lines can be useful to detect errors; If there is an error with the CUDA operation we get -1
     err = cudaGetLastError();
     if ( err != cudaSuccess )
      {
         printf("CUDA Error: %s\n", cudaGetErrorString(err));       
-
         // Possibly: exit(-1) if program cannot continue....
     }
 
+    //we assign the initial max value to the variables of gpu
     cudaMemcpy(global_minimum_gpu_opt, global_minimum_cpu, sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(global_minimum_gpu_fix, global_minimum_cpu, sizeof(int), cudaMemcpyHostToDevice);
     err = cudaGetLastError();
     if ( err != cudaSuccess )
      {
         printf("CUDA Error: %s\n", cudaGetErrorString(err));       
-
-        // Possibly: exit(-1) if program cannot continue....
     }
     
-    // Declare and initialize input array on CPU
+    // Declare and initialize input array on CPU; this is just an example; the input array can be whatever
     int input_array_cpu[M * N];
     for (int i = 0; i < M * N; i++) {
         input_array_cpu[i] = i + 2; 
@@ -64,24 +63,17 @@ int main() {
         err = cudaGetLastError();
     if ( err != cudaSuccess )
      {
-        printf("CUDA Error: %s\n", cudaGetErrorString(err));       
-
-        // Possibly: exit(-1) if program cannot continue....
+        printf("CUDA Error: %s\n", cudaGetErrorString(err));
     }
     cudaMemcpy(input_array_gpu, input_array_cpu, M * N * sizeof(int), cudaMemcpyHostToDevice); 
-    
-    
-        err = cudaGetLastError();
+    err = cudaGetLastError();
     if ( err != cudaSuccess )
      {
         printf("here CUDA Error: %s\n", cudaGetErrorString(err));       
-
-        // Possibly: exit(-1) if program cannot continue....
     }
     cudaDeviceSynchronize();
 
-    // Launch kernel
-    //find_minimum<<<NUM_BLOCKS, NUM_THREADS_PER_BLOCK>>>(input_array_gpu, global_minimum_gpu);
+    //We loop and we a lot of time and aunch kernel; at the end we converge and we get the minimum
     auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 1000000; i++) {
 
@@ -90,8 +82,6 @@ int main() {
         if ( err != cudaSuccess )
         {
             printf("CUDA Error: %s\n", cudaGetErrorString(err));       
-
-            // Possibly: exit(-1) if program cannot continue....
         }
         cudaDeviceSynchronize();
 
@@ -101,7 +91,7 @@ int main() {
     // Copy result back to CPU
     cudaMemcpy(global_minimum_cpu, global_minimum_gpu_opt, sizeof(int), cudaMemcpyDeviceToHost); cudaDeviceSynchronize();
     // Output the result
-    std::cout << "Computed minimum value by opt: " << global_minimum_cpu[0] << "  , in "<< duration.count() << "  seconds" <<std::endl;
+    std::cout << "Computed minimum value by the optimistic approach: " << global_minimum_cpu[0] << "  , in "<< duration.count() << "  seconds" <<std::endl;
     
     
     start = std::chrono::high_resolution_clock::now();
@@ -110,8 +100,10 @@ int main() {
     end = std::chrono::high_resolution_clock::now();
     duration = end - start;
     cudaMemcpy(global_minimum_cpu, global_minimum_gpu_opt, sizeof(int), cudaMemcpyDeviceToHost); cudaDeviceSynchronize();
-    std::cout << "Computed minimum value by fix: " << global_minimum_cpu[0] << "  , in "<< duration.count() << "  seconds" <<std::endl;
+    std::cout << "Computed minimum value by fix point approach: " << global_minimum_cpu[0] << "  , in "<< duration.count() << "  seconds" <<std::endl;
 
+    //The following is just a sligtly different implementation which exploit the synchronization between threads to find the minimum
+    //I left it on purpose because I think it can be interesting
     start = std::chrono::high_resolution_clock::now();
     find_minimum_test<<<NUM_BLOCKS, NUM_THREADS_PER_BLOCK>>>(input_array_gpu, global_minimum_gpu_fix);
     cudaDeviceSynchronize();
@@ -125,6 +117,8 @@ int main() {
     cudaFree(input_array_gpu);
 }
 
+//The following is just a sligtly different implementation which exploit the synchronization between threads to find the minimum
+//I left it on purpose because I think it can be interesting
 __global__ void find_minimum_test(int *input, int *global_minimum) {
     int index = blockDim.x*blockIdx.x + threadIdx.x;
     for(int i = 0; i < 100000; i++){
@@ -134,7 +128,7 @@ __global__ void find_minimum_test(int *input, int *global_minimum) {
     }
     //atomicExch(global_minimum, input[index]);
 }
-//optimistic approach
+//fix approach
 __global__ void find_minimum_fix(int *input, int *global_minimum) {
     int index = blockDim.x*blockIdx.x + threadIdx.x;
     if (input[index] < global_minimum[0])
